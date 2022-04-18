44c93c94acd782d73bf4287631e2b0328eb6fc84
David Sastre <d.sastresanemeterio@metro-market.de>
2022-04-18 18:19:08 +0200

    Recreate project

--- profile_project/asgi.py
+++ profile_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for profileSite project.
+ASGI config for profile_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'profileSite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'profile_project.settings')
 
 application = get_asgi_application()
